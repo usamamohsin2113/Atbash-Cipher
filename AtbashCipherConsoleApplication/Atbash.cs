67c30dade67a4a8d30b702d5fe744cf570683796
usama.mohsin.3975 <usama.mohsin.3975@slashnext.com>
2020-04-11 14:18:52 +0500

    Add project files.

---
+++ AtbashCipherConsoleApplication/Atbash.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtbashCipherConsoleApplication
+{
+    class Atbash
+    {
+        //This method will be used to cipher or decipher the given value
+        string EncrypterAndDecrypter(string value)
+        {
+            string encrypted = "";
+
+            //Loop through each chracter in the given string
+            foreach (char item in value)
+            {
+                if (item>=65 && item<=90)
+                {
+                    encrypted+=(char)('Z'-item+65);
+                }
+                else if (item>=97&&item<=122)
+                {
+                    encrypted+=(char)('z'-item+97);
+                }                
+            }
+            return encrypted;
+        }
+
+
+        // This method will take plain text and returns cipher of it
+        public string Encrypt(string value)
+        {
+            return this.EncrypterAndDecrypter(value);
+        }
+
+        // This method will take cipher text and produce decipher of it
+        public string Decrypt(string value)
+        {
+            return this.EncrypterAndDecrypter(value);
+        }
+    }
+}
---
+++ AtbashCipherConsoleApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtbashCipherConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            Atbash ob = new Atbash();
+            Console.Write("Enter value to be encrypted : ");
+            value = Console.ReadLine();
+
+            Console.WriteLine(ob.Encrypt(value));
+            Console.WriteLine(ob.Decrypt(ob.Encrypt(value)));
+        }
+    }
+}
